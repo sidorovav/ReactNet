b2bf44099b45bc46a9838fc83cb69f6495a0c604
Сидоров Александр Валерьевич <sidorovav@gmail.com>
2019-10-14 22:15:19 +0300

    пример

--- WebApplication1/Content/Hello.jsx
+++ WebApplication1/Content/Hello.jsx
@@ -1,5 +1,17 @@
 ﻿class Hello extends React.Component {
     render() {
-        return <h2>Пациент {this.props.name}</h2>;
+        return (<h2> Пациент {this.props.name}</h2>);
+        
+    }
+}
+
+class BlogPostExcerpt extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Title</h1>
+                <p>Description</p>
+            </div>
+        )
     }
 }
